f94b9f849a2302b0d5c4b621a22e240e81549a2b
Saurav Mehrotra <saurav24081996@gmail.com>
2016-10-06 00:54:16 +0530

    Edited position of hamburger and upgraded sidebar.

--- application/static/application/js/dashboard.js
+++ application/static/application/js/dashboard.js
@@ -1,25 +1,22 @@
 function hamburgerClick() {
     if(document.getElementById("sidebar").style.width == "250px"){
-        document.getElementById("sidebar").style.display = "block";
         document.getElementById("sidebar").style.width = "50px";
         document.getElementById("main").style.marginLeft = "50px";
         document.getElementById("sideLinks").style.display = "none";    
         document.getElementById("main").style.backgroundColor = "#F0F690";    
     } 
     else{
-        document.getElementById("sidebar").style.display = "block";
         document.getElementById("sidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";  
         document.getElementById("sideLinks").style.display = "block";   
         document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.3)";
     }
 }
-function outsideClick() {
-    if(document.getElementById("sidebar").style.width == "250px"){
-        document.getElementById("sidebar").style.display = "block";
+window.onclick = function(event) {
+    if (!event.target.matches('#sidebar') && !event.target.matches('#hamburger')) {
         document.getElementById("sidebar").style.width = "50px";
         document.getElementById("main").style.marginLeft = "50px";
         document.getElementById("sideLinks").style.display = "none";    
-        document.getElementById("main").style.backgroundColor = "#F0F690";    
+        document.getElementById("main").style.backgroundColor = "#F0F690"; 
     }
-}
+} 
